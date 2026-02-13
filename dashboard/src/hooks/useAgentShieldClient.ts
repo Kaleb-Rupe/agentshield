7e5ee135870492b0716046a330a5dc3abb6b53da
MightyMags <cure.pet.blond@cloak.id>
2026-02-13 03:44:08 -0500

    Migrating Dashboard to its own repo

--- dashboard/src/hooks/useAgentShieldClient.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useContext } from "react";
-import { AnchorContext } from "@/components/providers/AnchorProvider";
-
-export function useAgentShieldClient() {
-  const { client, program } = useContext(AnchorContext);
-  return { client, program };
-}
--- dashboard/src/hooks/useNetwork.ts
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { useContext } from "react";
-import { NetworkContext } from "@/components/providers/NetworkProvider";
-
-export function useNetwork() {
-  return useContext(NetworkContext);
-}
